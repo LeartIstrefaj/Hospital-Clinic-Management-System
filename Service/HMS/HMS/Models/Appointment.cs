ebf8b818864a405dbb980119eaac360a8294da4a
miranrexhepi <44025690+MiranRexhepi@users.noreply.github.com>
2023-06-19 18:57:52 +0200

    mongoDB init

---
+++ Service/HMS/HMS/Models/Appointment.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Serializers;
+using System;
+
+namespace HMS.Models
+{
+    [BsonIgnoreExtraElements]
+    public class Appointment
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+
+        [BsonElement("patientName")]
+        public string PatientName { get; set; } = string.Empty;
+
+        [BsonElement("date")]
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime Date { get; set; }
+
+        [BsonElement("email")]
+        public string Email { get; set; }
+
+        [BsonElement("doctorName")]
+        public string DoctorName { get; set; }
+
+        [BsonElement("roomNo")]
+        public int RoomNo { get; set; }
+    }
+}
---
+++ Service/HMS/HMS/Services/AppointmentService.cs
@@ -0,0 +1,42 @@
+﻿using HMS.Models;
+using System.Collections.Generic;
+using MongoDB.Driver;
+
+namespace HMS.Services
+{
+    public class AppointmentService : IAppointmentService
+    {
+        private readonly IMongoCollection<Appointment> _appointments;
+
+        public AppointmentService(IAppointmentDatabaseSettings settings, IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName);
+            _appointments = database.GetCollection<Appointment>(settings.AppointmentHMSCollectionName);
+        }
+        public Appointment Create(Appointment appointment)
+        {
+            _appointments.InsertOne(appointment);
+            return appointment;
+        }
+
+        public List<Appointment> Get()
+        {
+            return _appointments.Find(appointment => true).ToList();
+        }
+
+        public Appointment Get(string id)
+        {
+            return _appointments.Find(appointment => appointment.Id == id).FirstOrDefault();
+        }
+
+        public void Remove(string id)
+        {
+            _appointments.DeleteOne(appointment => appointment.Id == id);
+        }
+
+        public void Update(string id, Appointment appointment)
+        {
+            _appointments.ReplaceOne(appointment => appointment.Id == id, appointment);
+        }
+    }
+}
